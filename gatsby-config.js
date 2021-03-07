f69e9185d3a43ae4d2fecfa7c01311a4047dab53
karanveer <karan_4496@hotmail.com>
2021-03-07 11:42:36 -0500

    Fixed img path

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/img`,
         name: `images`,
       },
     },
